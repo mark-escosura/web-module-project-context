dd17a8235483520946d47bbf73790c5480a6956f
mark-escosura <escosuramarkse@gmail.com>
2021-11-04 12:45:22 -0700

    Task 2: Added Item Functionality(addItem) into my App.js & Created the Product Context folder

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import { BrowserRouter as Router, withRouter } from 'react-router-dom';
+
 import './sass/index.scss';
 import App from './App';
 
